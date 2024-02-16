817ab82677b350f7de86e98c078296473d12817e
shreyasshettyb <shreyasb52@gmail.com>
2024-02-16 14:45:27 +0530

    Initial Maven Project

---
+++ DLTE-Java/src/main/java/basics/java/App.java
@@ -0,0 +1,13 @@
+package basics.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
